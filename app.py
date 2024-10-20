e6cbe15a155906e972264d343340b29bb08ff9ff
surisaran <surisaran9@gmail.com>
2024-10-20 18:20:51 +0530

    Changed v db

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from assistant import Assistant
 import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_chroma import Chroma
+#from langchain_chroma import Chroma
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
 import logging
